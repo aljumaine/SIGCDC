8db1972afac1de5c84412ae5c921c8eef00aff71
Justine Nguyen <justinexnguyen@gmail.com>
2023-02-13 12:53:43 +0000

    feat: add screenshot modal and link to download image

--- src/js/screenshot.js
+++ src/js/screenshot.js
@@ -2,11 +2,42 @@ import $ from "jquery";
 import html2canvas from "html2canvas";
 
 const displayEl = document.getElementById("display-artifacts");
-const screenshotCanvasEl = document.getElementById("screenshot-canvas");
+const screenshotContainerEl = document.getElementById("screenshot-container");
+const screenshotDownloadEl = document.getElementById("screenshot-download");
+const $screenshotModal = $("#screenshot-modal");
+const $root = $("#root")
 
 $("#screenshot-button").on("click", function () {
+  toggleScreenshotModal();
+
   html2canvas(displayEl).then((canvas) => {
-    screenshotCanvasEl.innerHTML = "";
-    screenshotCanvasEl.appendChild(canvas);
+    screenshotContainerEl.innerHTML = "";
+    canvas.id = "screenshot-canvas";
+    screenshotContainerEl.appendChild(canvas);
+    createScreenshotImage(canvas, screenshotDownloadEl);
   });
 });
+
+$(".modal-close").on("click", () => {
+  toggleScreenshotModal();
+});
+
+const toggleScreenshotModal = () => {
+  $screenshotModal.toggleClass("hidden");
+
+  !$screenshotModal.hasClass("hidden") ? $root.addClass('scroll-none') : $root.removeClass('scroll-none')
+};
+
+const createScreenshotImage = (canvas, linkEl) => {
+  const image = canvas
+    .toDataURL("image/png", 1.0)
+    .replace("image/png", "image/octet-stream");
+  linkEl.download = "screenshot.png";
+  linkEl.href = image;
+};
+
+// Todo
+// display a modal window
+// x button to hide modal window
+// add button to download image
+// https://www.sanwebe.com/snippet/downloading-canvas-as-image-dataurl-on-button-click
