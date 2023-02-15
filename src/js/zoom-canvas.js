c8da66bb99cc76ef21536cdacf38652996f4feee
Justine Nguyen <justinexnguyen@gmail.com>
2023-02-15 14:33:09 +0000

    feat: controls ui layout

--- src/js/zoom-canvas.js
+++ src/js/zoom-canvas.js
@@ -2,23 +2,20 @@ import $ from "jquery";
 
 const displayEl = $("#display-artifacts");
 
-$("#zoom-button").on('click', () => {
-  var zoom = displayEl.data("zoom") || 100;
-  $("#zoom-label").text(zoom + "%");
-  
-  $("#zoom-slider").slider({
-      min: 10,
-      max: 500,
-      value: zoom,
-      step: 0.01,
-      slide: function(_, ui) {
-        const zoom = ui.value;
-        displayEl.css({
-          transform: "scale(" + zoom / 100 + ")",
-          "transform-origin": "0px 0px"
-        });
-        displayEl.data("zoom", zoom);
-        $("#zoom-label").text(zoom + "%");
-      }
-  });
+var zoom = displayEl.data("zoom") || 100;
+$(".global-zoom-label").text(zoom + "%");
+$(".global-zoom-slider").slider({
+  min: 10,
+  max: 500,
+  value: zoom,
+  step: 0.01,
+  slide: function (_, ui) {
+    const zoom = ui.value;
+    displayEl.css({
+      transform: "scale(" + zoom / 100 + ")",
+      "transform-origin": "0px 0px",
+    });
+    displayEl.data("zoom", zoom);
+    $(".global-zoom-label").text(zoom + "%");
+  },
 });
