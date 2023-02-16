f8463cdbeb113e8af3227713739be19c23b28cd3
Justine Nguyen <justinexnguyen@gmail.com>
2023-02-16 17:34:22 +0000

    feat: fix zoom width for canvas

--- src/js/zoom-canvas.js
+++ src/js/zoom-canvas.js
@@ -1,7 +1,14 @@
 import $ from "jquery";
 
 const displayEl = $("#display-artifacts");
-
+const calculateWidth = (zoom) => {
+  if (zoom < 40) {
+    return ` calc(100% + ${zoom * 2}%)`;
+  } else if (zoom < 100) {
+    return `calc(100% + ${zoom}%)`;
+  }
+  return "100%";
+};
 var zoom = displayEl.data("zoom") || 100;
 $(".global-zoom-label").text(zoom + "%");
 $(".global-zoom-slider").slider({
@@ -14,6 +21,7 @@ $(".global-zoom-slider").slider({
     displayEl.css({
       transform: "scale(" + zoom / 100 + ")",
       "transform-origin": "0px 0px",
+      width: calculateWidth(zoom),
     });
     displayEl.data("zoom", zoom);
     $(".global-zoom-label").text(zoom + "%");
